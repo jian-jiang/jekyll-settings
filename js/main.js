a0dee78d605de4ff31bf983967f20664df258565
Jianan Jiang <jianan.jiang@student.unsw.edu.au>
2014-12-04 07:46:45 +0800

    with comment disabled

--- js/main.js
+++ js/main.js
@@ -25,27 +25,4 @@ $(function(){
   rssBtn.on('click',function(){
     window.location.href=rssLink.attr('href');
   });
-  var language="en_US";
-  if (navigator.language) {
-    language = navigator.language;
-  }
-  else  if(navigator.browserLanguage) {
-    language =navigator.browserLanguage;
-  }
-  var l= new String(language);
-  var lr="";
-  lr=l[0]+l[1];
-  console.log(lr);
-
-  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-  var disqus_shortname = 'jiananjiang'; // required: replace example with your forum shortname
-  var disqus_config = function () {
-    this.language = lr;
-  };
-  /* * * DON'T EDIT BELOW THIS LINE * * */
-  (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-  })();
 });
